3af9d4d8630f8eb5876afc3f9e9e5cd3432caee5
aqmal pangestu <pangestuaqmal@gmail.com>
2019-05-11 23:10:21 +0700

    alhamdulillah

--- opencv.py
+++ opencv.py
@@ -3,87 +3,41 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
-
     # blurred the image with gaussian blur
     blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
-
-    # define color in HSV
-    lower_blue = np.array([101,157,0])
-    upper_blue = np.array([111,255,255])
-
-    lower_red = np.array([166, 84, 141])
-    upper_red = np.array([186,255,255])
-
-    lower_green = np.array([40,176,0])
-    upper_green = np.array([80,255,255])
-
     # Convert BGR to HSV
     hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+    
+    colors = {'Red color':(0,0,255), 'Green color':(0,255,0), 'Blue color':(255,0,0)}
+    lower = {'Red color':(166, 84, 141), 'Green color':(40,176,0), 'Blue color':(97, 100, 117)}
+    upper = {'Red color':(186,255,255), 'Green color':(80,255,255), 'Blue color':(117,255,255)}
+    
+    for key, value in upper.items():
+        kernel = np.ones((7,7),np.uint8)
+        mask = cv2.inRange(hsv, lower[key], upper[key])
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        center = None
+        contours,_  = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        for pic, contour in enumerate(contours):
+            approx = cv2.approxPolyDP(contour, 0.02*cv2.arcLength(contour, True), True)
+            x = approx.ravel()[0]
+            y = approx.ravel()[1]
+            if len(contours) > 0:
+                area = cv2.contourArea(contour)
+                M = cv2.moments(area)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                if(area>300): #for removing small noises
+                    cv2.drawContours(frame, [approx], 0, colors[key], 2)
+                    cv2.putText(frame,key,(int(x), int(y)),cv2.FONT_HERSHEY_SIMPLEX, 0.7,colors[key])
+                    if len(approx) == 3:
+                        cv2.putText(frame,"           TRIANGLE", (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.7,colors[key])
+                    elif len(approx) == 4:
+                        cv2.putText(frame,"           RECTANGLE", (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.7,colors[key])
+                    elif 10 < len(approx) < 20:
+                        cv2.putText(frame,"           CIRCLE", (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.7,colors[key])
 
-    # Threshold the HSV image to get only specific colors
-    red = cv2.inRange(hsv, lower_red, upper_red)
-    blue = cv2.inRange(hsv, lower_blue, upper_blue)
-    green = cv2.inRange(hsv, lower_green, upper_green)
-
-    _,redThreshold = cv2.threshold(red, 240, 255, cv2.THRESH_BINARY)
-        
-    _,blueThreshold = cv2.threshold(blue, 240, 255, cv2.THRESH_BINARY)
-
-    _,greenThreshold = cv2.threshold(green, 240, 255, cv2.THRESH_BINARY)
-
-    contours,_ = cv2.findContours(redThreshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        approx = cv2.approxPolyDP(contour, 0.02*cv2.arcLength(contour, True), True)
-        x = approx.ravel()[0]
-        y = approx.ravel()[1]
-        if(area>300): #for removing small noises
-            cv2.drawContours(frame, [approx], 0, (0,0,255), 3)
-            cv2.putText(frame,"RED color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,0,255))
-            if len(approx) == 3:
-                cv2.putText(frame, "          TRIANGLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,0,255))
-            elif len(approx) == 4:
-                cv2.putText(frame, "          RECTANGLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,0,255))
-            elif 10 < len(approx) < 20:
-                cv2.putText(frame, "          CIRCLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,0,255))      
-
-    contours,_ = cv2.findContours(greenThreshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        approx = cv2.approxPolyDP(contour, 0.02*cv2.arcLength(contour, True), True)
-        x = approx.ravel()[0]
-        y = approx.ravel()[1]
-        if(area>400): #for removing small noises
-            cv2.drawContours(frame, [approx], 0, (0,255,0), 3)
-            cv2.putText(frame,"GREEN color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,255,0))
-            if len(approx) == 3:
-                cv2.putText(frame, "            TRIANGLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,255,0))
-            elif len(approx) == 4:
-                cv2.putText(frame, "            RECTANGLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,255,0))
-            elif 10 < len(approx) < 20:
-                cv2.putText(frame, "            CIRCLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,255,0))
-
-
-    contours,_ = cv2.findContours(blueThreshold,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    for pic, contour in enumerate(contours):
-        area = cv2.contourArea(contour)
-        approx = cv2.approxPolyDP(contour, 0.02*cv2.arcLength(contour, True), True)
-        x = approx.ravel()[0]
-        y = approx.ravel()[1]
-        if(area>500):
-            cv2.drawContours(frame, [approx], 0, (255,0,0), 3)
-            cv2.putText(frame,"BLUE color",(x,y),cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0))
-            if len(approx) == 3:
-                cv2.putText(frame, "            TRIANGLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0))
-            elif len(approx) == 4:
-                cv2.putText(frame, "            RECTANGLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0))
-            elif 10 < len(approx) < 20:
-                cv2.putText(frame, "            CIRCLE", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0))
-            
-
-    cv2.imshow('red',redThreshold)
-    cv2.imshow('green',greenThreshold)
-    cv2.imshow('blue',blueThreshold)
+    cv2.imshow('warna',mask)
     cv2.imshow('frame',frame)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
