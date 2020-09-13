39c769e0f25cf6cf76fcd7018d95e055011a5eee
aqumaru <pangestuaqmal@gmail.com>
2020-09-13 14:59:07 +0700

    Add files via upload

--- tes.py
+++ tes.py
@@ -1,93 +1,27 @@
-from collections import deque 
-import io
-import time
-
-#import picamera
-import cv2 
+import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
-while(1):
-    # Take each frame%
-    _, frame = cap.read() 
+while True:
+    _, frame = cap.read()
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+     
+    lower = np.array([1, 165, 215])
+    high = np.array([21, 185, 295])
 
-    # Convert BGR to HSV
+    mask = cv2.inRange(hsv_frame, lower, high)
+    color = cv2.bitwise_and(frame, frame, mask=mask)
+    contours,_  = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        approx = cv2.approxPolyDP(contour, 0.03*cv2.arcLength(contour, True), True)
+        x = approx.ravel()[0]
+        y = approx.ravel()[1]
+        if len(contours) > 2:
+            cv2.putText(frame,"DROP ZONE", (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(0,0,255))
 
-
-    #defined blue
-
-    lower_blue = np.array([110, 50, 60], dtype=np.uint8)
-    upper_blue = np.array([130,255,255], dtype=np.uint8)
-    blue = [lower_blue, upper_blue, 'blue']
-
-    #defined red
-    lower_red = np.array([160, 90, 50], dtype=np.uint8)
-    upper_red = np.array([179,255,255], dtype=np.uint8)
-    red = [lower_red, upper_red, 'red']
-
-
-    #defined green
-    lower_green = np.array([60, 110, 60], dtype=np.uint8)
-    upper_green = np.array([160,255,255], dtype=np.uint8)
-    green = [lower_green, upper_green, 'green']
-
-
-    #defined yellow
-    lower_yellow = np.array([5, 110, 100], dtype=np.uint8)
-    upper_yellow = np.array([50, 255, 255], dtype=np.uint8)
-    yellow = [lower_yellow, upper_yellow, 'yellow']
-
-
-
-
-
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-
-
-
-    if  blue:
-        print ("blue")
-    # Threshold the HSV image to get only blue colors mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-    # Bitwise-AND mask and original image
-        res = c v2.bitwise_and(frame,frame, mask= mask)
-        #------------------------------------------------------------------------------#
-        # define range of red color in HSV
-
-    elif red:
-        print ("red")
-        # Threshold the HSV image to get only red colors
-        mask = cv2.inRange(hsv, lower_red, upper_red)
-
-        # Bitwise-AND mask and original image
-        res = cv2.bitwise_and(frame,frame, mask= mask)
-#        -------------------------------------------------------------------------------
-        # define range of green color in HSV
-
-    elif green:
-        print ("green")
-        # Threshold the HSV image to get only green colors
-        mask = cv2.inRange(hsv, lower_green, upper_green)
-
-        # Bitwise-AND mask and original image
-        res = cv2.bitwise_and(frame,frame, mask= mask)
-#        -------------------------------------------------------------------------------------
-    elif yellow:
-            print ("yellow")
-    # Threshold the HSV image to get only green colors
-            mask = cv2.inRange(hsv,lower_yellow, upper_yellow )
-
-    # Bitwise-AND mask and original image
-            res = cv2.bitwise_and(frame,frame, mask= mask)
-
-
-
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Color", color)
+    key = cv2.waitKey(1)
+    if key == 27:
         break
-    cv2.destroyAllWindows()
